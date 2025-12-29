07563eec33180fd5dc976aee6ba6322d334e506d
saksham gupta <guptasaksham2510@gmail.com>
2025-12-29 14:40:13 +0530

    implemented HeaderAuthenticationFilter to get roles from header passed through api-gateway

---
+++ hotel-service/src/main/java/com/hotel/service/security/HeaderAuthenticationFilter.java
@@ -0,0 +1,45 @@
+package com.hotel.service.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+public class HeaderAuthenticationFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        String email = request.getHeader("X-User-Email");
+        String role = request.getHeader("X-User-Role");
+
+        if (email != null && role != null) {
+
+            SimpleGrantedAuthority authority =
+                    new SimpleGrantedAuthority(role);
+
+            UsernamePasswordAuthenticationToken authentication =
+                    new UsernamePasswordAuthenticationToken(
+                            email,
+                            null,
+                            List.of(authority)
+                    );
+
+            SecurityContextHolder.getContext()
+                    .setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
